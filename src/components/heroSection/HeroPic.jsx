963469116dae7c8434bb4f8ba38d7f24fce73702
thanhbinh21 <iambinhage17@gmail.com>
2025-10-19 23:46:12 +0700

    fix: update image paths for consistency and improve accessibility

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,12 +12,11 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-  src="../../public/images/HexaPic.png"
-  alt="Abdelrahman Qassem"
-  className="max-h-[450px] w-auto rounded-full relative z-10 shadow-lg shadow-cyan-400
+        src="/images/HexaPic.png"
+        alt="binhdev"
+        className="max-h-[450px] w-auto rounded-full relative z-10 shadow-lg shadow-cyan-400
              animate-[pulse_2s_ease-in-out_infinite,wiggle_3s_ease-in-out_infinite]"
-/>
-
+      />
 
       <div className=" absolute -z-10 flex justify-center items-center animate-pulse ">
         <PiHexagonThin className=" md:h-[90%] sm:h-[120%] min-h-[600px] w-auto text-cyan blur-md animate-[spin_20s_linear_infinite] " />
